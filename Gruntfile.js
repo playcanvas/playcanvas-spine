f605df2b928cd653fb4017d4447a048f13b0c593
Will Eastcott <will@playcanvas.com>
2020-02-08 20:36:02 +0000

    Clean up examples. Re-org src files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,44 @@
 module.exports = function (grunt) {
-    const sourceFiles = [
-        'contrib/spine-ts/build/3.6/spine-core.js',
-        'src/plugin/spine.js',
-        'src/plugin/component/spine-component-system.js',
-        'src/plugin/component/spine-component.js',
-        'src/plugin/component/spine-component-data.js',
-        'src/plugin/plugin.js',
+    const srcCommon = [
+        'src/spine.js',
+        'src/component/spine-component-system.js',
+        'src/component/spine-component.js',
+        'src/component/spine-component-data.js',
+        'src/mesh-plugin.js',
     ];
+    const src36 = [
+        'contrib/spine-ts/build/3.6/spine-core.js',
+    ].concat(srcCommon);
+    const src38 = [
+        'contrib/spine-ts/build/3.8/spine-core.js',
+    ].concat(srcCommon);
+
     const banner = '/* Copyright 2015-2020 PlayCanvas Ltd */\n';
 
     grunt.initConfig({
         concat: {
-            js: {
+            spine36: {
                 options: {
                     banner: banner,
                     separator: '\n'
                 },
-                src: sourceFiles,
+                src: src36,
                 dest: 'lib/playcanvas-spine.3.6.js'
+            },
+            spine38: {
+                options: {
+                    banner: banner,
+                    separator: '\n'
+                },
+                src: src38,
+                dest: 'lib/playcanvas-spine.3.8.js'
             }
         },
         uglify: {
             build: {
                 files: {
-                    'lib/playcanvas-spine.3.6.min.js': sourceFiles
+                    'lib/playcanvas-spine.3.6.min.js': src36,
+                    'lib/playcanvas-spine.3.8.min.js': src38
                 }
             },
             options: {
