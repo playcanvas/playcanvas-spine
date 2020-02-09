ca46a0e52d90748f35e9cd4b34eed62f6c31f489
Will Eastcott <will@playcanvas.com>
2020-02-09 19:30:20 +0000

    Get Essential 3.8 exports working

--- src/spine.js
+++ src/spine.js
@@ -1,4 +1,51 @@
 pc.extend(pc, function () {
+    // Compare semantic versions
+    function versionCompare(v1, v2, options) {
+        var lexicographical = options && options.lexicographical,
+            zeroExtend = options && options.zeroExtend,
+            v1parts = v1.split('.'),
+            v2parts = v2.split('.');
+
+        function isValidPart(x) {
+            return (lexicographical ? /^\d+[A-Za-z]*$/ : /^\d+$/).test(x);
+        }
+
+        if (!v1parts.every(isValidPart) || !v2parts.every(isValidPart)) {
+            return NaN;
+        }
+
+        if (zeroExtend) {
+            while (v1parts.length < v2parts.length) v1parts.push("0");
+            while (v2parts.length < v1parts.length) v2parts.push("0");
+        }
+
+        if (!lexicographical) {
+            v1parts = v1parts.map(Number);
+            v2parts = v2parts.map(Number);
+        }
+
+        for (var i = 0; i < v1parts.length; ++i) {
+            if (v2parts.length == i) {
+                return 1;
+            }
+
+            if (v1parts[i] == v2parts[i]) {
+                continue;
+            }
+            else if (v1parts[i] > v2parts[i]) {
+                return 1;
+            }
+            else {
+                return -1;
+            }
+        }
+
+        if (v1parts.length != v2parts.length) {
+            return -1;
+        }
+
+        return 0;
+    }
 
     var ATTACHMENT_TYPE = {
         NULL: 0,
@@ -85,6 +132,7 @@ pc.extend(pc, function () {
         var json = new spine.SkeletonJson(new spine.AtlasAttachmentLoader(atlas));
         json.scale *= 0.01;
         var _skeletonData = json.readSkeletonData(skeletonData);
+        this.skeletonVersion = _skeletonData.version;
         this.skeleton = new spine.Skeleton(_skeletonData);
         this.skeleton.updateWorldTransform();
 
@@ -263,15 +311,19 @@ pc.extend(pc, function () {
                         material.depthWrite = false;
                         material.cull = pc.CULLFACE_NONE;
                         material.blendType = pc.BLEND_PREMULTIPLIED;
+
+                        if (versionCompare(this.skeletonVersion, "3.6.0") <= 0) {
+                            // override premultiplied chunk because images are already premultiplied
+                            // however the opacity is not premultiplied by slot alpha
+                            var alphaPremul = [
+                                'gl_FragColor.rgb *= vVertexColor.a;',
+                                'gl_FragColor.a = dAlpha;'
+                            ].join('\n');
+                            material.chunks.outputAlphaPremulPS = alphaPremul;
+                        }
+
                         material.update();
 
-                        // override premultiplied chunk because images are already premultiplied
-                        // however the opacity is not premultiplied by slot alpha
-                        var alphaPremul = [
-                            'gl_FragColor.rgb *= vVertexColor.a;',
-                            'gl_FragColor.a = dAlpha;'
-                        ].join('\n');
-                        material.chunks.outputAlphaPremulPS = alphaPremul;
                         this._materials[key] = material;
                     }
                     slot.material = key;
