511dfb1cb937cd1e17b9d56b0e70abaff5f6acbb
Martin Valigursky <59932779+mvaligursky@users.noreply.github.com>
2021-08-12 06:37:37 -0700

    [Fix] Fixes memory getting release properly (#48)
    Co-authored-by: Martin Valigursky <mvaligursky@snapchat.com>

--- build/playcanvas-spine.3.8.js
+++ build/playcanvas-spine.3.8.js
@@ -8838,6 +8838,7 @@ pc.extend(pc, function () {
             'spine'
         ];
 
+        this.on('beforeremove', this.onBeforeRemove, this);
         pc.ComponentSystem.bind('update', this.onUpdate, this);
     };
     SpineComponentSystem.prototype = Object.create(pc.ComponentSystem.prototype);
@@ -8849,7 +8850,7 @@ pc.extend(pc, function () {
             pc.ComponentSystem.prototype.initializeComponentData.call(this.system, component, data, properties);
         },
 
-        removeComponent: function (entity) {
+        onBeforeRemove: function (entity, component) {
             var data = entity.spine.data;
             if (data.spine) {
                 data.spine.destroy();
