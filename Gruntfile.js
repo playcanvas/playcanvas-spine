baca8cce6a7a9a1c1eaf7376eb56b431e6cbeeb8
Ed Preston <epreston@prestonsoft.com>
2023-08-05 09:03:17 +1000

    Update Build to support 4.1
    Name files based on max supported version

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,21 @@
 module.exports = function (grunt) {
     const srcCommon = [
-        'src/spine.js',
         'src/component/spine-component-system.js',
         'src/component/spine-component.js',
         'src/component/spine-component-data.js',
         'src/spine-plugin.js'
     ];
     const src36 = [
-        'contrib/spine-ts/build/3.6/spine-core.js'
+        'contrib/spine-ts/build/3.6/spine-core.js',
+        'src/spine36.js'
     ].concat(srcCommon);
     const src38 = [
-        'contrib/spine-ts/build/3.8/spine-core.js'
+        'contrib/spine-ts/build/3.8/spine-core.js',
+        'src/spine36.js'
+    ].concat(srcCommon);
+    const src41 = [
+        'contrib/spine-ts/build/4.1/spine-core.js',
+        'src/spine41.js'
     ].concat(srcCommon);
 
     const banner = '/* Copyright 2015-2021 PlayCanvas Ltd */\n';
@@ -32,13 +37,22 @@ module.exports = function (grunt) {
                 },
                 src: src38,
                 dest: 'build/playcanvas-spine.3.8.js'
+            },
+            spine41: {
+                options: {
+                    banner: banner,
+                    separator: '\n'
+                },
+                src: src41,
+                dest: 'build/playcanvas-spine.4.1.js'
             }
         },
         uglify: {
             build: {
                 files: {
                     'build/playcanvas-spine.3.6.min.js': src36,
-                    'build/playcanvas-spine.3.8.min.js': src38
+                    'build/playcanvas-spine.3.8.min.js': src38,
+                    'build/playcanvas-spine.4.1.min.js': src41
                 }
             },
             options: {
